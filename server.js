a8b49a5478a9919489b67166a113acf288d4b9b8
Dinesh Kumar <dineshkumar1228@gmail.com>
2019-02-17 22:55:32 +0530

    deploy updates to heroku2

--- server.js
+++ server.js
@@ -4,20 +4,12 @@ const path = require('path');
 
 const app = express();
 
-module.exports = {
-    entry: ['./app.js'],
-    output: {
-      path: __dirname + '/build',
-      filename: 'bundle.js'
-    }
-  }
-
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/sfdcApp'));
+app.use(express.static('./dist/sfdcApp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/sfdcApp/index.html'));
+res.sendFile(path.join(__dirname,'/dist/sfdcApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
