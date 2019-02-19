36b84c3c9536c52da87a4374c1d680ac05c6a9d2
Dinesh Kumar <dineshkumar1228@gmail.com>
2019-02-19 20:48:23 +0530

    push changes v7

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.use(express.static('./dist/sfdcApp'));
 
 
 
-app.post('/signedrequest', function(req,res) {
+/**app.post('/signedrequest', function(req,res) {
     var json = decode(req.body.signed_request, '3B148A2F76272D9C2557037B7C15824EA977A6A53D149350BB2B1EAA5A612C1A');
-});
+});**/
 
 
-/**app.get('/*', function(req,res) {
+app.get('/*', function(req,res) {
     
     res.sendFile(path.join(__dirname,'/dist/sfdcApp/index.html'));
-});**/
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
