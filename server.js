eae2c97e31c35ae128c5e258586af5f5e998bb9a
Dinesh Kumar <dineshkumar1228@gmail.com>
2019-02-19 21:46:38 +0530

    push changes v2

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+var decode = require('signed-request');
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/sfdcApp'));
 
 
 
-/**app.post('/signedrequest', function(req,res) {
+app.post('/signedrequest', function(req,res) {
     var json = decode(req.body.signed_request, '3B148A2F76272D9C2557037B7C15824EA977A6A53D149350BB2B1EAA5A612C1A');
-});**/
+});
 
 
 app.get('/', function(req,res) {
