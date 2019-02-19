e746fff6b10da4d27997987690ae91adb510a900
Dinesh Kumar <dineshkumar1228@gmail.com>
2019-02-19 20:31:30 +0530

    changes v9

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.post('/signedrequest', function(req,res) {
     var json = decode(req.body.signed_request, '3B148A2F76272D9C2557037B7C15824EA977A6A53D149350BB2B1EAA5A612C1A');
 });
 
-
-app.get('/*', function(req,res) {
+console.log('json -- '+json);
+/**app.get('/*', function(req,res) {
     
     res.sendFile(path.join(__dirname,'/dist/sfdcApp/index.html'));
-});
+});**/
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
